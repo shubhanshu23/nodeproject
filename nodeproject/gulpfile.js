10824b0ff0e828dea3be7f8df2f5080855cbd61b
Shubhanshu Singh <shubhanshu2392@gmail.com>
2017-08-09 14:05:41 +0000

    /adding new files

---
+++ nodeproject/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint')
+
+var jsFiles = ['*.js','src/**/*.js'];
+
+gulp.task('style',function(){
+    return gulp.src(jsFiles)
+    .pipe(jshint());
+    
+});
